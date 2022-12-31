6e80e4f2fba7a8d4fa6b250e8bbf908f0fbe54a6
Deven Manam <deven.manam@gmail.com>
2022-12-31 01:19:58 -0600

    ensure that cargo rebuilds when the bpf input is changed

--- methods-guest/build.rs
+++ methods-guest/build.rs
@@ -3,7 +3,12 @@
 // Licensed under the Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0>.
 // This file may not be copied, modified, or distributed except according to those terms.
 
+use std::path::Path;
+
 fn main() {
-    println!("cargo:rustc-link-search={}", std::env::var("BPF_LIB_DIR").unwrap());
+    let bpf_lib_dir = std::env::var("BPF_LIB_DIR").unwrap();
+    let bpf_lib_path = Path::new(&bpf_lib_dir).join("libbpf.a");
+    println!("cargo:rustc-link-search={}", bpf_lib_dir);
+    println!("cargo:rerun-if-changed=<{}", bpf_lib_path.to_str().unwrap());
     risc0_build::link();
 }
